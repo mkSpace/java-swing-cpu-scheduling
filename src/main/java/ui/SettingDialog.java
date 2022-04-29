17813071dc9e7ede35a18d05da3db19eab4a91cb
Jaemin.Park <once29@naver.com>
2022-04-29 23:23:45 +0900

    Create SettingDialog

--- src/main/java/ui/SettingDialog.java
+++ src/main/java/ui/SettingDialog.java
@@ -1,2 +1,22 @@
-package ui;public class SettingDialog {
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SettingDialog extends JDialog {
+
+    private static final int DIALOG_WIDTH = 720;
+    private static final int DIALOG_HEIGHT = 500;
+
+    public SettingDialog(JFrame owner, String title) {
+        super(owner, title);
+        setLayout(new FlowLayout());
+        setSize(new Dimension(DIALOG_WIDTH, DIALOG_HEIGHT));
+        setLocationRelativeTo(null);
+        setupViews();
+    }
+
+    private void setupViews() {
+
+    }
 }
